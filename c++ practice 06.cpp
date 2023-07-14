dc04a6d0d033142e22c50832b3756d5b7f3b3a82
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-14 16:21:45 +0600

    Add files via upload

---
+++ c++ practice 06.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main () {
+
+int num1;
+char name [20] ;
+
+cout<< "Enter the number :";
+
+
+cin>>num1;
+
+cout<< "Enter your name :";
+
+cin>>name;
+
+cout << "The number is :"<<num1 <<endl <<"Your name is :" <<name [20];
+
+getch();
+
+
+}
+
