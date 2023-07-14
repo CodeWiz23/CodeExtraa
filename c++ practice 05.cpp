94a40e06c217493bd21814625c61a62496f130f4
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-14 16:15:09 +0600

    Add files via upload

---
+++ c++ practice 05.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main () {
+
+int num1;
+cout<< "Enter the number :";
+
+cin>>num1;
+
+cout << "The number is :"<<num1 ;
+
+getch();
+
+
+
+
+
+
+
+
+}
