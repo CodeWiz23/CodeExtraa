e57c98fc09f2d8375e2670c98a84ecaca4ca4f80
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-14 15:49:12 +0600

    Add files via upload

---
+++ c++ practice 3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main (){
+
+int num1 =10 ,num2 = 20;
+
+float num3=30.30,num4 =40.40;
+
+char ch = 'p';
+
+
+cout << num1 <<endl << num2 <<endl << num3 <<endl << num4 <<endl << ch ;
+
+
+getch ();
+
+
+
+
+
+
+}
