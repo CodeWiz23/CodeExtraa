6d3574e791b6ef61ac0c8a72a7ffb4cad3818598
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-15 09:39:14 +0600

    Update tips.cpp

--- tips.cpp
+++ tips.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main ()
+{
+
+    int num ;
+    char ch ;
+    cout <<"Enter a number :";
+    cin >>  num;
+    cout << "Your number is : " <<num;
+    cout <<endl;
+
+    cout <<"Enter word:";
+    cin >> ch;
+    cout<<"Your word is :" <<ch;
+
+
+    getch();
+
+
+}
+
