b31a6163bb385c450833ca922f15a11b03c060e3
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-15 09:15:17 +0600

    Add files via upload

---
+++ pro 2 .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main ()
+{
+
+    int num ;
+    char ch ;
+cout <<"Enter a number :";
+cin >>  num;
+
+cout <<"Enter word:";
+cin >> ch;
+
+
+cout << "Your number is : " <<num <<endl <<"Your word is" <<ch;
+
+
+
+    getch();
+
+
+}
