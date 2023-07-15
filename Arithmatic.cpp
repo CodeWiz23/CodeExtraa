a8737537e1c15ff8d952214a63c460c2cf117e6a
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-15 10:03:51 +0600

    Add files via upload

---
+++ Arithmatic.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+#include <conio.h>
+
+using namespace std;
+
+int main ()
+{
+
+    int num1, num2;
+
+
+    cout << "Enter your numbers :";
+
+    cin>> num1>>num2;
+
+    int sum = num1 + num2 ;
+
+    cout<< "Your sum  is : " << sum ;
+
+    cout <<endl;
+
+
+
+    cout << "Enter your numbers :";
+
+    cin>> num1>>num2;
+
+    int sub = num1 - num2 ;
+
+    cout<< "Your sub is : " << sub ;
+
+
+
+    cout <<endl;
+
+
+
+    cout << "Enter your numbers :";
+
+    cin>> num1>>num2;
+
+    int multi = num1 * num2 ;
+
+    cout<< "Your multi is : " << multi ;
+
+
+
+    cout <<endl;
+
+
+
+    cout << "Enter your numbers :";
+
+    cin>> num1>>num2;
+
+    int div = num1 / num2 ;
+
+    cout<< "Your div is : " << div;
+
+
+    getch();
+
+
+}
+
+
+
