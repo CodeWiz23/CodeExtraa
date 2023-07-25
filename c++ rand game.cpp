1bd288bebc3392c36392f08571e7eca6790089f9
CodeWiz23 <139475346+CodeWiz23@users.noreply.github.com>
2023-07-25 20:31:57 +0600

    Add files via upload

---
+++ c++ rand game.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main ()
+{
+    int guess,randomNumber;
+
+    cout << "Enter your guess number 1 to 5 :" << " ";
+    cin >> guess;
+
+
+    randomNumber = rand () %5 +1 ;
+
+
+    if ( guess==randomNumber )
+
+    {
+        cout <<"You have won !";
+    }
+
+    else
+    {
+        cout << "You have lost !" <<endl;
+        cout << "The random number was : " <<randomNumber <<endl;
+    }
+
+    return 0;
+}
+
